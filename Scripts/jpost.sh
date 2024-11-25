d50d35b8ba33315f92b6f5a7831594bc4f23a18f
coolduderules <masterofpi3.14@gmail.com>
2024-11-25 10:31:59 -0500

    update

--- Scripts/jpost.sh
+++ Scripts/jpost.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-
+cd /home/jason
+git clone https://github.com/coolduderules/jasolinux.git
 cd /home/jason/jasolinux/Scripts
-echo -ne '1111\n1111\n1111\n1111\n' | ./install.sh -drs
+echo -ne '1111\n1111\n1111\n1111\n1111\n1111\n1111\n1111\n1111\n1111\n' | ./install.sh -drs
 #sed  -i "s/sudo .*/&\" root/;s/sudo /echo \-ne '1111\\\n' \|  su -c \"/" *.sh
-echo -ne '1\n' | ./install.sh -drs
 pacman -Qeqt > /home/jason/after.txt
 exit
