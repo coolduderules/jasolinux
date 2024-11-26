1338cd13dc2608fb4a504b52e8a9ecceea3a985d
coolduderules <masterofpi3.14@gmail.com>
2024-11-26 05:05:35 -0500

    update

--- Scripts/jpost.sh
+++ Scripts/jpost.sh
@@ -8,6 +8,6 @@ sudo mkinitcpio -P
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 echo -ne 'y\n' | ./install.sh -drs
 #sed  -i "s/sudo .*/&\" root/;s/sudo /echo \-ne '1111\\\n' \|  su -c \"/" *.sh
-rsync -axHAWXS /home/jason/jasolinux/Configs/* /home/jason/
+rsync -axHAWXS /home/jason/jasolinux/Configs/ /home/jason/
 pacman -Qeqt > /home/jason/after.txt
 exit
