c63e6bd56e9e2c0870e32925123f58a88d5cebb7
coolduderules <masterofpi3.14@gmail.com>
2024-11-26 00:57:02 -0500

    update

--- Scripts/jpost.sh
+++ Scripts/jpost.sh
@@ -3,6 +3,9 @@
 cd /home/jason
 git clone https://github.com/coolduderules/jasolinux.git
 cd /home/jason/jasolinux/Scripts
+sudo sysctl kernel.unprivileged_userns_clone=1
+sudo mkinitcpio -P
+sudo grub-mkconfig -o /boot/grub/grub.cfg
 echo -ne 'y\n' | ./install.sh -drs
 #sed  -i "s/sudo .*/&\" root/;s/sudo /echo \-ne '1111\\\n' \|  su -c \"/" *.sh
 rsync -axHAWXS /home/jason/jasolinux/Configs/* /home/jason/
