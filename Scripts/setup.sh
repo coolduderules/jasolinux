f0daaae6fae7235099cb144e149d7b70df21cce8
coolduderules <masterofpi3.14@gmail.com>
2024-12-31 11:56:05 +0000

    Dec 31, 2024, 6:56 AM

--- Scripts/setup.sh
+++ Scripts/setup.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 
+pacman --noconfirm -Syu
 pacman --noconfirm -U /home/jason/jasolinux/pkgs/apple-firmware-1-1-any.pkg.tar.zst
 systemctl enable iptables ip6tables systemd-networkd systemd-resolved iwd sshd
 
-pacman --noconfirm -Syu
 echo -ne 'y\ny\n' | pacman -S bubblewrap-suid
 mkinitcpio -P
 grub-mkconfig -o /boot/grub/grub.cfg
